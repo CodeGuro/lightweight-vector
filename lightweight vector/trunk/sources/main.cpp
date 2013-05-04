7ca732a706540c6a1e572c63be7610fe4c5b6b7e
Tony <CodeGuro@gmail.com>
2013-05-04 02:45:48 -0400

    vector functionality

--- lightweight vector/trunk/sources/main.cpp
+++ lightweight vector/trunk/sources/main.cpp
@@ -2,8 +2,18 @@
 #include <lightweight_vector.hpp>
 
 template class gstd::lightweight_vector< int >;
+template class gstd::_lightweight_vector_iterator<int>;
 
 int main(int const argc, char const * const * argv)
 {
+	gstd::lightweight_vector< unsigned, unsigned > b;
+	b.push_back(32);
+	b.push_back(49);
+	b.push_back(55);
+	auto it = b.begin();
+	++it;
+	++it;
+	unsigned r = *it;
+	b.erase( b.begin(), b.end() );
 	return EXIT_SUCCESS;
 }
